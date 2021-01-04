7b109edf791acecb5ac3f6a17bbf0e60dd064a38
20079934 <20079934@mail.wit.ie>
2021-01-04 16:58:00 +0000

    commiting this attempt to get drawer working

--- app/src/main/java/com/w20079934/mydiary/activities/MainActivity.kt
+++ app/src/main/java/com/w20079934/mydiary/activities/MainActivity.kt
@@ -1,28 +1,44 @@
 package com.w20079934.mydiary.activities
 
+import android.content.ClipData
 import android.os.Bundle
 import android.view.View
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.w20079934.mydiary.R
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.nav_header_main.*
-import kotlinx.android.synthetic.main.nav_header_main.view.*
+import com.w20079934.mydiary.main.MainApp
 import org.jetbrains.anko.AnkoLogger
 
 
 class MainActivity : AppCompatActivity(), AnkoLogger {
 
+    lateinit var app: MainApp
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        app = application as MainApp
+
         //i'm tired so i looked it up: https://stackoverflow.com/a/38418531/14543599
         val navigationView = findViewById<View>(R.id.navigation_view) as NavigationView
         val header = navigationView.getHeaderView(0);
         val textView = header.findViewById(R.id.diaryName) as TextView
-        textView.text = "IT WORKS OMG"
+        textView.text = "Hello ${app.entries.getName()}!"
+
+        val navController = findNavController(R.id.recyclerView)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.nav_newentry, R.id.nav_renamediary), drawerLayout)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navigationView.setupWithNavController(navController)
 
     }
 }
